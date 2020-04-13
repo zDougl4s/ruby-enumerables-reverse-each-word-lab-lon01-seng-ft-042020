5aff610805e0508ea78a3a352c7a8c4537e7b4a9
Douglas Manuel Barco Martin <douglas@dobuilding.co.uk>
2020-04-13 15:46:01 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,9 @@
+require "pry"
 def reverse_each_word(message)
   normalwords = message.split(" ")
   reverseWords = normalwords.collect do|i|
    i.reverse
     end
+    binding.pry
   reverseWords.join(" ")
 end
